f3a3a3112d3b215532ebfc57ce82fcd9f2c91076
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-03 15:58:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //your JS code here. If required.
 const sizeInfo=document.getElementById('sizeInfo');
 
-window.onresize=function (){
+window.addEventListener('resize',()=>{
 	const h1=document.createElement('h1');
 	h1.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
-}
+})
+
 
 
